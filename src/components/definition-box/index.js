7b94c9ff66c95e6e381c6370cee4bfad59d2dca5
Yisehak Abreham <yisehak.aw@gmail.com>
2020-08-05 10:51:09 +0300

    modify content of description box

--- src/components/definition-box/index.js
+++ src/components/definition-box/index.js
@@ -7,16 +7,15 @@ export default ({ nodes }) => {
       <Collapse bordered={false} defaultActiveKey={[0]}>
         {nodes.map((n, i) => (
           <Collapse.Panel header={n.data.id} key={i}>
-            <h3>
-              {n.data.id} - {n.data.name}
-            </h3>
+            <h3>{n.data.identifier}</h3>
             <p>{n.data.definition}</p>
+            <p>Location: {n.data.location ? n.data.location : "Unlocalized"}</p>
             <h4>Connections</h4>
             <ul>
               {n.connectedNodes.map((cn) => (
                 <li>
                   <a href={cn.data.definition} target="_blank">
-                    {cn.data.id} - {cn.data.name}
+                    {cn.data.id}
                   </a>
                 </li>
               ))}
