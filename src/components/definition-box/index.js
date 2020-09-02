3f394a08ec50c297a131e2f3dd77596f18bdfb1b
Yisehak Abreham <yisehak.aw@gmail.com>
2020-09-02 11:30:20 +0300

    make location and definition of nodes links

--- src/components/definition-box/index.js
+++ src/components/definition-box/index.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { Collapse } from "antd";
 
-export default ({ nodes }) => {
-  return (
-    <>
-      <Collapse bordered={false} defaultActiveKey={[0]}>
-        {nodes.map((n, i) => (
-          <Collapse.Panel header={n.data.identifier} key={i}>
-            <p>{n.data.definition}</p>
-            <p>Location: {n.data.location ? n.data.location : "Unlocalized"}</p>
-            <h3>Connections</h3>
-            <ul>
-              {n.connectedNodes.map((cn) => (
-                <li key={cn.data.id}>
-                  <a href={cn.data.definition} target="_blank">
-                    {cn.data.id}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </Collapse.Panel>
-        ))}
-      </Collapse>
-    </>
-  );
-};
+export default ({ nodes }) => (
+  <Collapse bordered={false} defaultActiveKey={[0]}>
+    {nodes.map((n, i) => (
+      <Collapse.Panel header={n.data.identifier} key={i}>
+        <p>
+          <a href={n.data.definition}>{n.data.definition}</a>
+        </p>
+        <p>
+          Location:{" "}
+          {n.data.location ? (
+            <a
+              target="_blank"
+              href={`http://amigo.geneontology.org/amigo/term/${n.data.location}`}
+            >
+              {n.data.location}
+            </a>
+          ) : (
+            "Unlocalized"
+          )}
+        </p>
+        <h3>Connections</h3>
+        <ul>
+          {n.connectedNodes.map((cn) => (
+            <li key={cn.data.id}>
+              <a href={cn.data.definition} target="_blank">
+                {cn.data.id}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </Collapse.Panel>
+    ))}
+  </Collapse>
+);
